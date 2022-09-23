be14f69299528fa8f39dac479b267499c9ae72a2
sunwei <418531356@qq.com>
2022-09-23 22:49:22 +0800

    add singleton pattern example

---
+++ design_pattern/singleton.py
@@ -0,0 +1,15 @@
+class Singleton:
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(cls, "_instance"):
+            cls._instance = super(Singleton, cls).__new__(cls)
+        return cls._instance
+
+class MyClass(Singleton):
+    def __init__(self, a):
+        self.a = a
+    
+a = MyClass(10)
+b = MyClass(20)
+print(a.a)
+print(b.a)
+print(id(a), id(b))
